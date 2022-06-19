31cb856ad74865a0073bafa8ef997f32f0471ac4
Mirek <Mirek@DESKTOP-ROAP5PV>
2022-06-19 20:04:15 +0200

    Dodaj pliki projektów.

---
+++ SredniaOcen/Program.cs
@@ -0,0 +1,10 @@
+﻿double srednia;
+Console.WriteLine("Podaj srednia ocen:");
+srednia = double.Parse(Console.ReadLine());
+
+if (srednia <=3.99)
+    Console.WriteLine("Twoje stypendium wynosi: 0pln");
+else if (srednia <= 4.79)
+    Console.WriteLine("Twoje stypendium wynosi: 350pln");
+else if (srednia >=4.80)
+    Console.WriteLine("Twoje stypendium wynosi: 550pln");
